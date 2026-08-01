42ae52e4837e657675be8d09f3e05cc4fb142fef
ervishalsinha <vishal8952sinha@gmail.com>
2026-08-01 10:50:08 +0530

    db endpoint updated

--- backend/app/api/routes/user.py
+++ backend/app/api/routes/user.py
@@ -17,7 +17,6 @@ from app.services.user_service import get_user_by_email
 from app.models.user import User
 from sqlalchemy import text
 from app.db.database import engine
-
 router = APIRouter()
 
 
@@ -79,12 +78,17 @@ def get_user(db: Session = Depends(get_db)):
 
 
 
-
 @router.get("/db-test")
 def db_test():
     try:
         with engine.connect() as conn:
-            conn.execute(text("SELECT 1"))
-        return {"status": "Database connected"}
+            result = conn.execute(text("SELECT version()"))
+            return {
+                "status": "Database connected",
+                "db_version": result.scalar()
+            }
     except Exception as e:
-        return {"error": str(e)}
+        return {
+            "status": "failed",
+            "error": str(e)
+        }
